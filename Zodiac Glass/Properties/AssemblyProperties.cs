a017ff73462ac3ddcde2ebd059a41eeb758b6c14
InvisibleShield <InvisibleShield.XIV@gmail.com>
2015-03-01 00:50:23 +0100

    * increment version

--- Zodiac Glass/Properties/AssemblyProperties.cs
+++ Zodiac Glass/Properties/AssemblyProperties.cs
@@ -2,7 +2,7 @@
 {
     internal static class AssemblyProperties
     {
-        internal const string Version = "1.3.1502.28000"; // X.X.Year_Year_Month_Mont.Day_Day_BuildOfDayBuildOfDayBuildOfDay
+        internal const string Version = "1.3.1503.01000"; // X.X.Year_Year_Month_Mont.Day_Day_BuildOfDayBuildOfDayBuildOfDay
         internal const string Name = "Zodiac Glass";
         internal const string Copyright = "© 2015 Invisible Shield";
     }
