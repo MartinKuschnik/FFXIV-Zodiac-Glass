fb02affa4ebbfc32e05899f75c94230115fb02c8
Martin Kuschnik <martin-kuschnik@live.com>
2015-11-15 14:41:14 +0100

    updated the version

--- Zodiac Glass/Properties/AssemblyProperties.cs
+++ Zodiac Glass/Properties/AssemblyProperties.cs
@@ -2,8 +2,8 @@
 {
     internal static class AssemblyProperties
     {
-        internal const string Version = "1.4.1507.15001"; // X.X.Year_Year_Month_Mont.Day_Day_BuildOfDayBuildOfDayBuildOfDay
+        internal const string Version = "1.4.1511.15001"; // X.X.Year_Year_Month_Mont.Day_Day_BuildOfDayBuildOfDayBuildOfDay
         internal const string Name = "Zodiac Glass";
-        internal const string Copyright = "© 2015 Invisible Shield";
+        internal const string Copyright = "© 2015 Martin Kuschnik";
     }
 }
