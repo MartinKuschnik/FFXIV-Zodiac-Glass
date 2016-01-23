8437e45f69cfe27ce4960680cc9a22bb58129d12
Martin Kuschnik <martin-kuschnik@live.com>
2016-01-23 18:17:09 +0100

    Memory address update for patch 3.15

--- Zodiac Glass/Properties/AssemblyProperties.cs
+++ Zodiac Glass/Properties/AssemblyProperties.cs
@@ -2,8 +2,8 @@
 {
     internal static class AssemblyProperties
     {
-        internal const string Version = "1.4.1511.20001"; // X.X.Year_Year_Month_Mont.Day_Day_BuildOfDayBuildOfDayBuildOfDay
+        internal const string Version = "1.4.1601.23001"; // X.X.Year_Year_Month_Mont.Day_Day_BuildOfDayBuildOfDayBuildOfDay
         internal const string Name = "Zodiac Glass";
-        internal const string Copyright = "© 2015 Martin Kuschnik";
+        internal const string Copyright = "© 2016 Martin Kuschnik";
     }
 }
