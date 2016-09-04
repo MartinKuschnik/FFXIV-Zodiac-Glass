f7bbae39b995890e1d1851c7077c0d84361cb633
TLNiles <niles.prower@gmail.com>
2016-09-04 15:39:09 +0300

    Update FFXIVMemoryMap.cs

--- Zodiac Glass/FFXIV/FFXIVMemoryMap.cs
+++ Zodiac Glass/FFXIV/FFXIVMemoryMap.cs
@@ -6,7 +6,7 @@
     public class FFXIVMemoryMap
     {
         private static readonly Lazy<FFXIVMemoryMap> _default = new Lazy<FFXIVMemoryMap>(() => new FFXIVMemoryMap() { ItemSetPointer =
-            new FFXIVPointer() { BaseAddressOffset = 0x010917A8, Offsets = new int[] { 0x40, 0x0 } }
+            new FFXIVPointer() { BaseAddressOffset = 0x01105540, Offsets = new int[] { 0x40, 0x0 } }
         });
 
         public static FFXIVMemoryMap Default
